cb9844e19dafbb145048fc0f1bc5ff8fed641698
Renan <renan_battlenet@hotmail.com>
2018-05-03 03:53:08 -0300

    Update

--- scripts/tab_manager.js
+++ scripts/tab_manager.js
@@ -3,8 +3,9 @@ function openTab(evt, tabName, element){
 		tabcontent,
 		tablinks;
 
+	document.getElementById("teacher-central-content").innerHTML="";
 	tabcontent = document.getElementsByClassName("tabcontent");
-	
+
 	for(i = 0; i < tabcontent.length; i++){
 		tabcontent[i].style.display = "none";
 	}
@@ -16,8 +17,9 @@ function openTab(evt, tabName, element){
 		tablinks[i].style.backgroundColor = " rgb(40,40,40)";
 	}
 
+	element.style.backgroundColor = " rgb(70,70,70)";
+
 	document.getElementById(tabName).style.display = "block";
 	evt.currentTarget.className += " active";
 
-	element.style.backgroundColor = " rgb(70,70,70)";
 }
